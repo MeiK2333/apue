d7a6e7e2b7de100e21110d69ac00048920b5495b
MeiK <meik2333@gmail.com>
2018-05-09 21:39:31 +0800

    update: Chapter05 标准 I/O 库

---
+++ Chapter-05/5.8.2.c
@@ -0,0 +1,17 @@
+#include "apue.h"
+
+int main() {
+    char buf[MAXLINE];
+
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if (fputs(buf, stdout) == EOF) {
+            err_sys("output error");
+        }
+    }
+
+    if (ferror(stdin)) {
+        err_sys("input error");
+    }
+
+    exit(0);
+}
--- Chapter-05/README.md
+++ Chapter-05/README.md
@@ -3,3 +3,19 @@
 
 ### 随书代码
 
+
+#### 5.8.c && 5.8.2.c
+
+```shell
+$ gcc 5.8.2.c -lapue
+$ ./a.out 
+Hello World!
+Hello World!
+^C
+$ gcc 5.8.c -lapue
+$ ./a.out 
+Hello World!
+Hello World!
+```
+
+书中指出：标准 I/O 库与直接调用 read 和 write 函数相比并不慢很多。
