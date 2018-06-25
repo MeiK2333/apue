cc1e456c397f2c53831fbbbeb583f3f1d756602d
MeiK <meik2333@gmail.com>
2018-06-25 20:08:56 +0800

    format code

---
+++ Chapter-10/10.3.c
@@ -0,0 +1,26 @@
+#include "apue.h"
+
+static void sig_usr(int);
+
+int main() {
+    if (signal(SIGUSR1, sig_usr) == SIG_ERR) {
+        err_sys("can't catch SIGUSR1");
+    }
+    if (signal(SIGUSR2, sig_usr) == SIG_ERR) {
+        err_sys("can't catch SIGUSR2");
+    }
+
+    for (;;) {
+        pause();
+    }
+}
+
+static void sig_usr(int signo) {
+    if (signo == SIGUSR1) {
+        printf("received SIGUSR1\n");
+    } else if (signo == SIGUSR2) {
+        printf("received SIGUSR2\n");
+    } else {
+        err_dump("received signal %d\n", signo);
+    }
+}
--- Chapter-10/README.md
+++ Chapter-10/README.md
@@ -1,6 +1,16 @@
-## 信号
+# 信号
 
+## 随书代码
 
-### 随书代码
+### 10.3.c
 
-
+```shell
+$ gcc 10.3.c -lapue
+$ ./a.out &
+[1] 18096
+$ kill -USR1 18096
+received SIGUSR1
+$ kill -USR2 18096
+received SIGUSR2
+$ kill 18096
+```
