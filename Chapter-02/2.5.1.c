427099348fa2e7ce1d33c6c6284853fea3d3e47d
nswsnb <55875979+nswsnb@users.noreply.github.com>
2024-04-03 10:39:00 +0800

    Update 2.5.1.c
    %d -> %u

--- Chapter-02/2.5.1.c
+++ Chapter-02/2.5.1.c
@@ -9,7 +9,7 @@ int main() {
     printf("SCHAR_MIN: %d\n", SCHAR_MIN);
     printf("INT_MAX: %d\n", INT_MAX);
     printf("INT_MIN: %d\n", INT_MIN);
-    printf("UINT_MAX: %d\n", UINT_MAX);
+    printf("UINT_MAX: %u\n", UINT_MAX);
     printf("SHRT_MAX: %d\n", SHRT_MAX);
     printf("SHRT_MIN: %d\n", SHRT_MIN);
     printf("USHRT_MAX: %d\n", USHRT_MAX);
